5a07aad9fe49421501a6d61e1eb8834919b91557
plugin10 <lukiwrzs@o2.pl>
2024-11-24 11:48:49 +0100

    Add JwtTokenGenerator class

---
+++ ModelStore.API/TokenGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace ModelStore.Identity
+{
+    public class TokenGenerator
+    {
+        public string GenerateToke(int id, string email)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            // on production store this key securely
+            var key = "MySecretTokenGeneratorKey"u8.ToArray();
+
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new(JwtRegisteredClaimNames.Sub, email),
+                new(JwtRegisteredClaimNames.Email, email),
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(60),
+                Issuer = "https://user.localhost",
+                Audience = "https://localhost",
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
