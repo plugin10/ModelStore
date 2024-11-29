06185e56950eb5e689ede63e406fec5746d020af
plugin10 <lukiwrzs@o2.pl>
2024-11-29 22:50:08 +0100

    Add interceptor for auth with jwt token.

--- modelstore-frontend/src/app/app.config.ts
+++ modelstore-frontend/src/app/app.config.ts
@@ -2,14 +2,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { authInterceptor } from './shared/services/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom([BrowserAnimationsModule]),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
   ],
 };
