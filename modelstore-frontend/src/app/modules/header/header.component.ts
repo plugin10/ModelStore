bdb938db40c3baf3d3e9d95f39ede3bd1e366d96
plugin10 <lukiwrzs@o2.pl>
2024-12-01 17:16:27 +0100

    Fix view for header component

--- modelstore-frontend/src/app/modules/header/header.component.ts
+++ modelstore-frontend/src/app/modules/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
@@ -11,6 +11,8 @@ import { getMenuItems } from './menu-items';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { LoginComponent } from '../login/login.component';
 import { ButtonModule } from 'primeng/button';
+import { AuthService } from '../../shared/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -27,19 +29,37 @@ import { ButtonModule } from 'primeng/button';
     ButtonModule,
   ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   items: MenuItem[] | undefined;
   ref: DynamicDialogRef | undefined;
 
-  constructor(public router: Router, public dialogService: DialogService) {}
+  isLoggedIn = false;
+  userRole: string | null = null;
+  private subscriptions: Subscription = new Subscription();
+
+  constructor(
+    public router: Router,
+    public dialogService: DialogService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.items = getMenuItems(this);
-  }
 
-  showTasks() {}
+    this.subscriptions.add(
+      this.authService.getLoggedInStatus().subscribe((status) => {
+        this.isLoggedIn = status;
+      })
+    );
+
+    this.subscriptions.add(
+      this.authService.getUserRoleStatus().subscribe((role) => {
+        this.userRole = role;
+      })
+    );
+  }
 
   showLoginDialog() {
     this.ref = this.dialogService.open(LoginComponent, {
@@ -51,9 +71,20 @@ export class HeaderComponent {
         '640px': '95vw',
       },
     });
+
     this.ref.onClose.subscribe((login: boolean) => {
       if (login) {
+        console.log('Użytkownik zalogowany');
       }
     });
+  }
+
+  logout() {
+    this.authService.clearToken();
+    console.log('Użytkownik wylogowany');
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 }
