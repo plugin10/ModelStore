6eff346a6e76a69494cd62a661514ad491571ded
plugin10 <lukiwrzs@o2.pl>
2024-12-07 19:23:43 +0100

    Add cart component

--- modelstore-frontend/src/app/app.routes.ts
+++ modelstore-frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { ProductsComponent } from './modules/products/products.component';
+import { CartComponent } from './modules/cart/cart.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'cart', component: CartComponent },
   { path: '**', redirectTo: '' },
 ];
---
+++ modelstore-frontend/src/app/modules/cart/cart.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CartItem } from '../../shared/interfaces/cart-item';
+import { MessageService } from 'primeng/api';
+import { CartService } from '../../shared/services/cart.service';
+import { Subscription } from 'rxjs';
+import { Guid } from 'guid-typescript';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { ToastModule } from 'primeng/toast';
+import { TableModule } from 'primeng/table';
+import { Product } from '../../shared/interfaces/product';
+
+@Component({
+  selector: 'app-cart',
+  standalone: true,
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+  imports: [ButtonModule, RippleModule, ToastModule, TableModule],
+  providers: [MessageService],
+})
+export class CartComponent implements OnInit, OnDestroy {
+  cartItems: CartItem[] = [];
+  private subscriptions: Subscription = new Subscription();
+
+  constructor(
+    private cartService: CartService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.subscriptions.add(
+      this.cartService.getCart().subscribe((cart) => {
+        this.cartItems = cart;
+      })
+    );
+  }
+
+  increaseQuantity(product: Product): void {
+    console.log(product);
+    this.cartService.addToCart(product);
+  }
+
+  decreaseQuantity(productId: Guid): void {
+    console.log(productId);
+    this.cartService.decreaseQuantity(productId);
+  }
+
+  removeFromCart(productId: Guid): void {
+    this.cartService.removeFromCart(productId);
+  }
+
+  clearCart(): void {
+    this.cartService.clearCart();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+}
